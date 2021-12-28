2c1a61fe071837af9d151631faf61142098e40d6
9935358984yaar <79708844+9935358984yaar@users.noreply.github.com>
2021-12-28 14:52:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-video = "";  
 status = "";
 
 function preload(){
   video = createVideo('video.mp4');
+  video.hide();
 }
 
 
 function setup() {
   canvas = createCanvas(480, 380);
   canvas.center();
-  video.hide();
 }
 
 function start()
